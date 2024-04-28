76b8e9a43520cda0d23d46b6f6a4bce720e3ff75
Emilion <emiljadersten@gmail.com>
2024-04-28 20:03:51 +0200

    Fix bugs with the arrow buttons

--- script.js
+++ script.js
@@ -1,56 +1,63 @@
-const sections = document.querySelectorAll('section');
-const upArrow = document.getElementById('up-arrow');
-const downArrow = document.getElementById('down-arrow');
+const sections = document.querySelectorAll("section");
+const upArrow = document.getElementById("up-arrow");
+const downArrow = document.getElementById("down-arrow");
+const arrowsMinWidth = 687;
 
 // Toggle Navbar Menu
 function toggleMenu() {
-    const menu = document.querySelector(".nav-links");
-    const icon = document.querySelector(".burger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+  const menu = document.querySelector(".nav-links");
+  const icon = document.querySelector(".burger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
 
 // Determine visibility of navigation arrows
 const updateArrowsVisibility = () => {
-    const scrollPosition = window.scrollY;
-    const windowHeight = window.innerHeight;
-    const pageHeight = document.body.scrollHeight;
+  const scrollPosition = window.scrollY;
+  const windowHeight = window.innerHeight;
+  const pageHeight = document.body.scrollHeight;
 
-    const distanceFromBottom = pageHeight - (scrollPosition + windowHeight);
-
+  const distanceFromBottom = pageHeight - (scrollPosition + windowHeight);
+  if (window.innerWidth >= arrowsMinWidth) {
     if (scrollPosition > 450) {
-        upArrow.style.display = "block";
+      upArrow.style.display = "block";
     } else {
-        upArrow.style.display = "none";
+      upArrow.style.display = "none";
     }
     if (distanceFromBottom > 400) {
-        downArrow.style.display = "block";
+      downArrow.style.display = "block";
     } else {
-        downArrow.style.display = "none";
+      downArrow.style.display = "none";
     }
+  }
 };
 
+// Initial visibility check on page load
+updateArrowsVisibility();
+
 // Add event listener to scroll event
-window.addEventListener('scroll', () => {
-    updateArrowsVisibility();
+window.addEventListener("scroll", () => {
+  updateArrowsVisibility();
 });
 
 // Add event listener to the 'previous' arrow
-upArrow.addEventListener('click', () => {
-    Array.from(sections).reverse().forEach(section => {
-        const rect = section.getBoundingClientRect();
-        if (rect.top <= window.innerHeight && rect.bottom > 0) {
-            section.scrollIntoView({ behavior: 'smooth', block: 'end' });
-        }
+upArrow.addEventListener("click", () => {
+  Array.from(sections)
+    .reverse()
+    .forEach((section) => {
+      const rect = section.getBoundingClientRect();
+      if (rect.top <= window.innerHeight && rect.bottom > 0) {
+        section.scrollIntoView({ behavior: "smooth", block: "end" });
+      }
     });
 });
 
 // Add event listener to the 'next' arrow
-downArrow.addEventListener('click', () => {
-    sections.forEach(section => {
-        const rect = section.getBoundingClientRect();
-        if (rect.top >= 0 && rect.top < window.innerHeight) {
-            section.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        }
-    });
+downArrow.addEventListener("click", () => {
+  sections.forEach((section) => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top >= 0 && rect.top < window.innerHeight) {
+      section.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  });
 });
